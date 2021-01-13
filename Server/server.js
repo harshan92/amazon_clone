59bead85df5dfc781579c9ae89c86da200e1eda2
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2021-01-14 01:04:30 +0530

    added user create post req

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 const bodyParser=require("body-parser")
 const mongoose=require("mongoose");
 const dotenv=require("dotenv")
+const User=require("./models/user")
+
 dotenv.config()
 const app = express()
 
@@ -22,7 +24,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const port = 3000
 
 app.get('/', (req, res) => res.json('Hello World!'))
-app.post('/', (req, res) => console.log(req.body.name))
+app.post('/', (req, res) =>{
+    let user=new User();
+    user.name=req.body.name
+    user.email=req.body.email
+    user.password=req.body.password
+    user.save(err=>{
+        if(err){
+            res.json(err)
+        }else{
+            res.json("Successfully saved!")
+        }
+    })
+})
 app.listen(port, error=>{
     if(error) console.log(error);
     else console.log("Server is running on port "+port);
